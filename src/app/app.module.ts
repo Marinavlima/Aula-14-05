9c57b37f47c06fe04b1ae36f154911d5a9387bbc
Marina Viana <marininhavl_@hotmail.com>
2022-06-04 17:12:35 -0300

    atualização 04-06

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{ReactiveFormsModule}from'@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,11 +9,13 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { JogosComponent } from './jogos/jogos.component';
-import { ClienteComponent } from './cliente/cliente.component';
+import { FormularioComponent } from './clientes/formulario/formulario.component';
 
 //import de API
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -21,14 +24,16 @@ import {MatButtonModule} from '@angular/material/button';
     HomeComponent,
     FooterComponent,
     JogosComponent,
-    ClienteComponent
+    FormularioComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
